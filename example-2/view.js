7ba35b11949b5f0499668dcd79734b57ca8c6df8
Barney Wilton <B.Wilton1993@gmail.com>
2023-05-31 11:43:33 +0100

    dynamically generated second paragraph on index.html

--- example-2/view.js
+++ example-2/view.js
@@ -4,6 +4,12 @@ class View {
 
     console.log(this.mainContainerEl);
   }
+
+  addParagraph = () => {
+    let newPara = document.createElement('p');
+    newPara.textContent = 'This paragraph has been dynamically added by JavaScript!';
+    this.mainContainerEl.append(newPara);
+  }
 }
 
 module.exports = View;
