faeb5df7ad514e53f205c29ed9dda08adf2168e6
Barney Wilton <B.Wilton1993@gmail.com>
2023-05-31 14:08:14 +0100

    Wrote test for two notes added

--- notes-app/notesView.test.js
+++ notes-app/notesView.test.js
@@ -10,21 +10,28 @@ describe(NotesView, () => {
   beforeEach(() => {
     document.body.innerHTML = fs.readFileSync('./index.html');
   });
-
+  
   it('displays nothing when no notes added', () => {
     const model = new NotesModel();
     const notesView = new NotesView(model);
-
     notesView.displayNotes();
     expect(document.querySelectorAll('div .note').length).toBe(0);
   })
-
+  
   it('displays one note when a note has been added', () => {
     const model = new NotesModel();
     const notesView = new NotesView(model);
-
     model.addNote('Note 1');
     notesView.displayNotes();
     expect(document.querySelectorAll('div .note').length).toBe(1);
+  })
+  
+  it('displays two notes when two notes have been added', () => {
+    const model = new NotesModel();
+    const notesView = new NotesView(model);
+    model.addNote('Note 1');
+    model.addNote('Note 2');
+    notesView.displayNotes();
+    expect(document.querySelectorAll('div .note').length).toBe(2);
   })
 })
